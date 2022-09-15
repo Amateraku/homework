aa8a27ba8c68817ada51ee1d859ecd7f8e503107
Amateraku <yaroslav.polyakoff@gmail.com>
2022-09-15 22:32:07 +0300

    complete exersize 110

--- task91_110.py
+++ task91_110.py
@@ -1,5 +1,16 @@
 def task91_110():
-    pass
+    file = open("names.txt")
+    names = [x.strip() for x in file.readlines()]
+    file.close()
+    user_name = input("Enter name to delete: ")
+    if user_name not in names:
+        print("Invalid name.")
+    else:
+        names.remove(user_name)
+        new_file = open("new_names.txt", "w")
+        for name in names:
+            new_file.write(f"{name}\n")
+        new_file.close()
 
 
 if __name__ == '__main__':
