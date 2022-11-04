dac26f6c8903d0eed1e59a690a9a987e493875f1
Amateraku <yaroslav.polyakoff@gmail.com>
2022-11-04 20:08:08 +0200

    my problem

---
+++ shift_code.py
@@ -0,0 +1,61 @@
+def main():
+    print(f"1) Make a code\n2) Decode a message\n3) Quit")
+    primer = ("a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m",
+              "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z")
+    punctuations = (".", ",", "?", "!", ":", ";", "(", ")", " ")
+    while True:
+        user_choose = input("Enter your selection: ")
+        if user_choose == "1":
+            make_a_code(primer, punctuations)
+        elif user_choose == "2":
+            decode_a_message(primer, punctuations)
+        elif user_choose == "3":
+            quit_code()
+        else:
+            print("Enter correct selection.")
+
+
+def make_a_code(primer, punctuations):
+    message = input("Enter message: ")
+    striped_message = [x for x in message]
+    finished_message = []
+    for letter in striped_message:
+        for letter2 in range(len(primer) - 1):
+            if letter == primer[len(primer) - 1]:
+                finished_message.append(primer[0])
+                break
+            elif letter == primer[letter2]:
+                finished_message.append(primer[letter2 + 1])
+                break
+            for punctuation in punctuations:
+                if letter == punctuation:
+                    finished_message.append(letter)
+                    break
+    print(finished_message)
+
+
+def decode_a_message(primer, punctuations):
+    message = input("Enter message: ")
+    striped_message = [x for x in message]
+    finished_message = []
+    for letter in striped_message:
+        for letter2 in range(len(primer) - 1):
+            if letter == primer[0]:
+                finished_message.append(primer[len(primer) - 1])
+                break
+            elif letter == primer[letter2]:
+                finished_message.append(primer[letter2 - 1])
+                break
+            for punctuation in punctuations:
+                if letter == punctuation:
+                    finished_message.append(letter)
+                    break
+    print(finished_message)
+
+
+def quit_code():
+    quit()
+
+
+if __name__ == '__main__':
+    main()
