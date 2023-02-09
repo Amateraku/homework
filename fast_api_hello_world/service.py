0d9636d29d949d7a865158deed4287e0348a8ccc
Amateraku <yaroslav.polyakoff@gmail.com>
2023-02-09 19:43:44 +0200

    update weather service

--- fast_api_hello_world/service.py
+++ fast_api_hello_world/service.py
@@ -1,4 +1,4 @@
-from starlette.responses import HTMLResponse
+from fastapi.responses import JSONResponse
 from fastapi import FastAPI
 import requests
 import json
@@ -10,8 +10,11 @@ WEATHER_API_TOKEN = "da00a2d85203422db90195136232501"
 
 @api.get("/weather")
 def weather(city):
+    error = json.dumps({"error": "Error has corrupted"})
     response = requests.get(f"https://api.weatherapi.com/v1/current.json?key=da00a2d85203422db90195136232501&q={city}&aqi=no")
-    location_info = json.loads(response.text)["location"]
-    weather_info = json.loads(response.text)["current"]
-    print(weather_info)
-    return HTMLResponse(f"<h1>In {location_info['name']} temperature is {weather_info['temp_c']}°C and humidity is {weather_info['humidity']}%.</h1>")
+    print(response)
+    try:
+        return JSONResponse(json.loads(response.text)["location"]["name"],
+                            json.loads(response.text))
+    finally:
+        return JSONResponse(json.loads(error)["error"])
