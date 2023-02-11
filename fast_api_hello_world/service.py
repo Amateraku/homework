a016715bf0aeaf9fc4706b4f7fbb815028fcede2
Amateraku <yaroslav.polyakoff@gmail.com>
2023-02-11 22:30:37 +0200

    working weather forecoaster

--- fast_api_hello_world/service.py
+++ fast_api_hello_world/service.py
@@ -11,10 +11,13 @@ WEATHER_API_TOKEN = "da00a2d85203422db90195136232501"
 @api.get("/weather")
 def weather(city):
     error = json.dumps({"error": "Error has corrupted"})
-    response = requests.get(f"https://api.weatherapi.com/v1/current.json?key=da00a2d85203422db90195136232501&q={city}&aqi=no")
-    print(response)
     try:
-        return JSONResponse(json.loads(response.text)["location"]["name"],
-                            json.loads(response.text))
-    finally:
+        response = requests.get(
+            f"https://api.weatherapi.com/v1/current.json?key=da00a2d85203422db90195136232501&q={city}&aqi=no")
+        info = {"city": json.loads(response.text)["location"]["name"],
+                "temp": json.loads(response.text)["current"]["temp_c"],
+                "humidity": json.loads(response.text)["current"]["humidity"]}
+        return JSONResponse(info)
+    except Exception as err:
+        print(err)
         return JSONResponse(json.loads(error)["error"])
