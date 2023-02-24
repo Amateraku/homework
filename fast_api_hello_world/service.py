d1d33f70a7681ba7889cf8db31fb8286757343d1
Amateraku <yaroslav.polyakoff@gmail.com>
2023-02-24 20:30:38 +0200

    update code PIP8

--- fast_api_hello_world/service.py
+++ fast_api_hello_world/service.py
@@ -3,21 +3,20 @@ from fastapi import FastAPI
 import requests
 import json
 
-
 api = FastAPI()
 WEATHER_API_TOKEN = "da00a2d85203422db90195136232501"
 
 
 @api.get("/weather")
 def weather(city):
-    error = json.dumps({"error": "Error has corrupted"})
+    error_messages = json.dumps({"error": "Error has corrupted"})
     try:
-        response = requests.get(
+        weather_info = requests.get(
             f"https://api.weatherapi.com/v1/current.json?key={WEATHER_API_TOKEN}={city}&aqi=no")
-        info = {"city": json.loads(response.text)["location"]["name"],
-                "temp": json.loads(response.text)["current"]["temp_c"],
-                "humidity": json.loads(response.text)["current"]["humidity"]}
-        return JSONResponse(info)
+        weather_for_user = {"city": json.loads(weather_info.text)["location"]["name"],
+                            "temp": json.loads(weather_info.text)["current"]["temp_c"],
+                            "humidity": json.loads(weather_info.text)["current"]["humidity"]}
+        return JSONResponse(weather_for_user)
     except Exception as err:
         print(err)
-        return JSONResponse(json.loads(error)["error"])
+        return JSONResponse(json.loads(error_messages)["error"])
